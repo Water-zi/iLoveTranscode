0a65117b51ae678df1045436c946de57dd812e85
Water-zi <928069358@qq.com>
2024-01-30 23:04:22 +0800

    Initial Commit

---
+++ iLoveTranscode/iLoveTranscodeApp.swift
@@ -0,0 +1,20 @@
+//
+//  iLoveTranscodeApp.swift
+//  iLoveTranscode
+//
+//  Created by 唐梓皓 on 2024/1/30.
+//
+
+import SwiftUI
+
+@main
+struct iLoveTranscodeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
